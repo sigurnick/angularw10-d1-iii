36fd52f4a923ca53a0092c17db17a3f5e020487f
sigurnick <nicolas94.m@gmail.com>
2023-07-24 15:27:29 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angularw10-d1-iii';
+}
