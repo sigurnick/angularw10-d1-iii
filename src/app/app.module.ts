c6a537a247984003e0729490791cc3a78cc4b642
sigurnick <nicolas94.m@gmail.com>
2023-07-24 16:36:49 +0200

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ArticleComponent } from './components/article/article.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ArticleComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
